3636df28c8e29418f823cf81cf63793ad26310b6
Emmanuel Rodriguez <erog2013@gmail.com>
2022-03-07 23:47:24 -0600

    Testing cluster mode

--- bin/01-init.sh
+++ bin/01-init.sh
@@ -13,6 +13,15 @@ gcloud services enable krmapihosting.googleapis.com \
 # gcloud compute firewall-rules create allow-ssh --network default --source-ranges 35.235.240.0/20 --allow tcp:22 --project=kcc-root 
 gcloud compute networks create default --subnet-mode=auto --project=kcc-root 
 
+# CREATE NAT ROUTER (TO ACCESS GITHUB)
+gcloud compute routers create default-nat-router --network default --region us-central1 --project kcc-root
+
+gcloud compute routers nats create default-nat-config --router-region us-central1 --router default-nat-router \
+--nat-all-subnet-ip-ranges --auto-allocate-nat-external-ips --project kcc-root
+
+## Test
+gcloud compute ssh <KCC_HOST> --zone us-central1-f  --tunnel-through-iap --project kcc-root 
+
 # CREATE CONTROLLER
 # --> https://cloud.google.com/anthos-config-management/docs/how-to/config-controller-setup
 
@@ -22,14 +31,6 @@ gcloud anthos config controller list
 export KUBECONFIG=$(pwd)/.kcc-kube.yaml   
 
 gcloud container clusters get-credentials krmapihost-root-cluster --region us-central1 --project kcc-root
-
-# CREATE NAT ROUTER (TO ACCESS GITHUB)
-gcloud compute routers create default-nat-router --network default --region us-central1 --project kcc-root
-
-gcloud compute routers nats create default-nat-config --router-region us-central1 --router default-nat-router \
---nat-all-subnet-ip-ranges --auto-allocate-nat-external-ips --project kcc-root
-## Test
-gcloud compute ssh <KCC_HOST> --zone us-central1-f  --tunnel-through-iap --project kcc-root 
  
 # PERMISSION
 export SA_EMAIL="$(kubectl get ConfigConnectorContext -n config-control -o jsonpath='{.items[0].spec.googleServiceAccount}' 2> /dev/null)"
